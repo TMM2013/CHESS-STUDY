aafa7beb4aad0196ced1b1106faa0dd0a8f25f5e
Joe Heffernan <jheffernan@mcw.edu>
2016-11-30 12:07:23 -0600

    Added file to easily print trials.

---
+++ NordicDesign/source/QuickPrintTrials.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+Run=$1
+DesignFile=/home/heffjos/Documents/EpisodicMemory/Design.csv
+
+i=1
+for Line in `grep -E ^${Run} ${DesignFile}`
+do
+    ImgName=`echo ${Line} | awk -F, '{print $5}'`
+    ImgName=IMG${ImgName/.jpg/}
+    TrialName=`printf Trial%04d ${i}`
+    echo "<Trial>"
+    echo "    <name>${TrialName}</name>"
+    echo "    <show>"
+    echo "        <item>${ImgName}</item>"
+    echo "    </show>"
+    echo "    <show>"
+    echo "        <item>NoImg</item>"
+    echo "    </show>"
+    echo "</Trial>"
+    echo
+    ((i++))
+done
