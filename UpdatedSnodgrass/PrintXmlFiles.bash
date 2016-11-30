3b1b1c9168c5e062223e38563676c42c3f4278f8
Joe Heffernan <jheffernan@mcw.edu>
2016-11-30 12:02:08 -0600

    PrintXmlFiles.bash complete.

--- UpdatedSnodgrass/PrintXmlFiles.bash
+++ UpdatedSnodgrass/PrintXmlFiles.bash
@@ -2,12 +2,19 @@
 
 for oneFile in *jpg
 do
+    # let's skip position right now
     output=${oneFile/.jpg/}
-    echo "<name>${output}</name>"
-    echo "<Picture>../../UpdatedSnodgrass/${oneFile}</Picture>"
-    echo "<duration>3000</duration>"
-    echo "<expectedResponse>"
-    echo "    <key>C</key>"
-    echo "    <count>0</count>"
-    echo "</expectedResponse>"
+    echo "<Slide>"
+    echo "    <name>IMG${output}</name>"
+    echo "    <condition>1</condition>"
+    # make sure path below is correct
+    echo "    <Picture>../../UpdatedSnodgrass/${oneFile}</Picture>"
+    echo "    <duration>2500</duration>"
+    echo "    <expectedResponse>"
+    echo "        <key>C</key>"
+    echo "        <count>1</count>"
+    echo "    </expectedResponse>"
+    echo "</Slide>"
+    echo 
+done
 
