c5a2d742f5aaf2f9da2a01ceec62bceb0bef7635
Joseph Heffernan <heffjos@umich.edu>
2016-12-14 13:34:07 -0600

    Added goodbye slide.

--- UpdatedSnodgrass/PrintSlides.bash
+++ UpdatedSnodgrass/PrintSlides.bash
@@ -2,20 +2,40 @@
 echo '<?xml version="1.0" encoding="UTF-8"?>'
 echo "<!-- Must be a top level tag to include all tags to include -->"
 echo "<IncludeSlides>"
+echo 
 
 # print intro slide
 echo "    <Slide>"
 echo "        <name>Directions</name>"
 echo "        <duration>10000</duration>"
-echo "        <text>You will see a series of images.</text>"
-echo "        <text>Some new, some prevously viewed during training.</text>"
-echo '        <text>Press the "C" key when you see images previously viewed during training.</text>'
+echo "        <text>You will see a series of images."
+echo "Some new, some prevously viewed during training."
+echo 'Press the "C" key when you see images previously viewed during training.</text>'
 echo "        <color>Black</color>"
 echo "        <font>Arial</font>"
 echo "        <fontsize>24</fontsize>"
-echo "        <backGroundColor>Black</backGroundColor>"
+echo "        <backGroundColor>White</backGroundColor>"
 echo "    </Slide>"
 echo 
+
+# print NoImg slide
+echo "    <Slide>"
+echo "        <name>NoImg</name>"
+echo "        <condition>3</condition>"
+echo "        <duration>500</duration>"
+echo "        <backGroundColor>White</backGroundColor>"
+echo "    </Slide>"
+echo
+
+# print Goodbye slide
+echo "    <Slide>"
+echo "        <name>Goodbye</name>"
+echo "        <text>Goodbye.</text>"
+echo "        <font>Arial</font>"
+echo "        <fontsize>24</fontsize>"
+echo "        <duration>1000</duration>"
+echo "    </Slide>"
+echo
 
 for oneFile in *jpeg
 do
